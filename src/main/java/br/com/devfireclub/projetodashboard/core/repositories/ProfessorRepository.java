4b30a6a5c2bc59b8a9c1647c97af0c5963ef48fa
aly50n <alyson.morato13@gmail.com>
2023-06-23 08:52:01 -0300

    Correção no tipo inserido no Jpa e na função find

--- src/main/java/br/com/devfireclub/projetodashboard/core/repositories/ProfessorRepository.java
+++ src/main/java/br/com/devfireclub/projetodashboard/core/repositories/ProfessorRepository.java
@@ -2,10 +2,10 @@ package br.com.devfireclub.projetodashboard.core.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.devfireclub.projetodashboard.core.exceptions.ProfessorNotFoundException;
-import br.com.devfireclub.projetodashboard.core.models.Aluno;
+import br.com.devfireclub.projetodashboard.core.models.Professor;
 
-public interface ProfessorRepository extends JpaRepository<Aluno, Long> {
-    default Aluno findByIdOrElseThrow(Long id){
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    default Professor findByIdOrElseThrow(Long id){
         return findById(id)
                         .orElseThrow(ProfessorNotFoundException::new);
     }
