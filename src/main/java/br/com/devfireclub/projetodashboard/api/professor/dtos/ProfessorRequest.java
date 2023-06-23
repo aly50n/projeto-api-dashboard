4bb93d3f859b9911f51592960f0adb86a881a3dd
aly50n <alyson.morato13@gmail.com>
2023-06-23 08:22:20 -0300

    feat: ProfessorRequest & Response implementados.

--- src/main/java/br/com/devfireclub/projetodashboard/api/professor/dtos/ProfessorRequest.java
+++ src/main/java/br/com/devfireclub/projetodashboard/api/professor/dtos/ProfessorRequest.java
@@ -1,5 +1,20 @@
 package br.com.devfireclub.projetodashboard.api.professor.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProfessorRequest {
+
+    private String nome;
+
+    private String curso;
+
+    private Long idEndereco;
     
 }
